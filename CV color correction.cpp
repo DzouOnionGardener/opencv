558ede333787b60ac55be9df73e3cc69c4009253
D.Zou <Dawenzou1@gmail.com>
2016-10-14 17:30:06 -0400

    color correction
    NA

---
+++ CV color correction.cpp
@@ -0,0 +1,24 @@
+// CV color correction.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <c:\openCV-3.1.0\opencv\build\include\opencv2\imgproc\imgproc.hpp>
+#include <c:\openCV-3.1.0\opencv\build\include\opencv2\core.hpp>
+#include <c:\openCV-3.1.0\opencv\build\include\opencv\highgui.h>
+#include <c:\openCV-3.1.0\opencv\build\include\opencv2\opencv.hpp>
+#include <c:\openCV-3.1.0\opencv\build\include\opencv\cv.h>
+#include <iostream>
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	Mat my_image = imread("bug.jpg");
+
+	imshow("bug", my_image);
+	cvWaitKey(0);
+
+	system("PAUSE");
+	return EXIT_SUCCESS;
+}
+
